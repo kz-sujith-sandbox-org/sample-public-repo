5041afc460e5ca832678872f451555a7c4b8b0e5
Sujith Cheruvil <127106402+SujithCheruvilKaizen@users.noreply.github.com>
2024-01-09 14:42:02 +0530

    Update vulnerable.py

--- vulnerable.py
+++ vulnerable.py
@@ -4,3 +4,10 @@ super_secret = "eyksdf00(xksdklskjjj"
 os.system(f"")
 
 print(super_secret)
+print(os.environ)
+
+exit(1)
+
+print("What the hell is in this script???")
+
+os.system(f"eval {input("")}")
